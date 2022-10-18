72d34d2246b193eb4f5f021cc9b0a716e2c31985
Israel Casillas Jr <ICJR@users.noreply.github.com>
2022-10-18 02:02:12 -0700

    refactor: move whitelist from replace to validation package

--- src/local/index.ts
+++ src/local/index.ts
@@ -155,21 +155,7 @@ class Store {
         });
     }
 
-    replace(values: { [key: string]: any }, whitelist: string[] = []): void {
-        if (whitelist.length) {
-            let whitelisted: { [key: string]: any } = {};
-
-            for (let i = 0, n = whitelist.length; i < n; i++) {
-                if (!values[whitelist[i]]) {
-                    continue;
-                }
-        
-                whitelisted[whitelist[i]] = values[whitelist[i]];
-            }
-
-            values = whitelisted;
-        }
-
+    replace(values: { [key: string]: any }): void {
         if (!Object.keys(values).length) {
             return;
         }
